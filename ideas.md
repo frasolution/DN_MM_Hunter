1fdbb0a2e51a6b5dda5d56c12ab149982973175e
Max M <meshstyles@users.noreply.github.com>
2018-12-10 00:36:18 +0100

    Ideas and first proof of concept

---
+++ ideas.md
@@ -0,0 +1,21 @@
+# IDEAS
+
+## Use ArrayList for the creation of Hunters and Prey
+* Create objects for n objects with ArrayList <as seen in ./knowledge_base-samples/objArrTest.java>
+* Enter n via UI (use slider)
+* use arraylist or array
+  
+## create Virtual class Actor
+* visabiltyCone()
+* movement()
+* isFriendly()
+* attack()
+* movement speed defined
+
+## HunterActor
+* isInGroup()
+* howling()
+* isGroupNeeded()
+
+## PreyActor
+* canRetaliate()
---
+++ knowledge_base-samples/objArrTest.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class objArrTest{
+    private int number;
+    private String name;
+    // actually not required here but I want to have it for cpy pst
+    public void setName(String in){
+		name = in;
+	}
+    public void setNumber(int in){
+		number = in;
+    }
+    public int getNumber(){
+        return number;
+    }
+    public String getName(){
+        return name;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); //just for demonstartion
+        int arrsize = sc.nextInt(); //can grab the number of objects that need to be crated from ui
+        objArrTest [] arrs = new objArrTest[arrsize]; 
+        // create an array for objects to be saved in. USE ARRAYLIST FOR THIS IN FINAL FOR EASY EXPANSION
+        for(int i = 0 ; i < arrsize ; i++ ){
+            arrs[i] = new objArrTest(); //create new object
+            arrs[i].setName( "test Object " + i ); //set name of object
+            arrs[i].setNumber( i ); //set number of object
+        }
+        for(int i = 0 ; i < arrsize ; i++ ){
+            System.out.println("id : " + arrs[i].getNumber() + " name : " + arrs[i].getName() ); //sysout for testing
+        }
+    }
+}
